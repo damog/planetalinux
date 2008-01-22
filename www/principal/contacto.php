a7ea2a979bd6b072329d5794d73cb0a6b7fd29ed
damog <damog@b249013b-0064-4a1a-be8d-188746b68b0a>
2008-01-22 01:10:42 +0000

    Agrego informacion y desinformacion.
    git-svn-id: svn+ssh://menteslibres.org/home/planetalinux/svn@29 b249013b-0064-4a1a-be8d-188746b68b0a

--- www/principal/contacto.php
+++ www/principal/contacto.php
@@ -8,7 +8,9 @@
 	<p>Planeta Linux es trabajado desde una lista de correo. Si te interesa ponerte en contacto con nosotros, hazlo por favor
 	a la dirección <b><a href="mailto:planetalinux@googlegroups.com">planetalinux@googlegroups.com</a></b>.</p>
 	
-	Si te interesa ser miembro del grupo y la lista de correos, puedes hacerlo libremente en la dirección <a href="http://groups.google.com/group/planetalinux">http://groups.google.com/group/planetalinux</a>.</p>
+	<p>Además, es común que nos encontremos en IRC chateando y perdiendo el tiempo más de la costumbre, en <tt>#planetalinux</tt> de <tt>irc.freenode.net</tt>.</p>
+
+	Si te interesa ser parte del equipo de administración, es bueno que te suscribas a la lista de correos. <strong>NO es necesario darte de alta en la lista para aparecer en los blogrolls de cada una de las instancias</strong>. La lista de correos es específicamente para los que mantenemos las instancias desde las tripas. Si aún así quieres pertenecer a ese grupo de locos, puedes hacerlo libremente en la dirección <a href="http://groups.google.com/group/planetalinux">http://groups.google.com/group/planetalinux</a>.</p>
 </fieldset>
 </div>
 </div>
