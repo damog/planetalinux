13c3c6002d198e2314229d5cfb54219ab7a4c0a9
gnuget <gnuget@b249013b-0064-4a1a-be8d-188746b68b0a>
2008-06-09 00:09:06 +0000

    Agregue banderas al index de las instancias que faltaban
    git-svn-id: svn+ssh://menteslibres.org/home/planetalinux/svn@132 b249013b-0064-4a1a-be8d-188746b68b0a

--- www/principal/index.php
+++ www/principal/index.php
@@ -8,9 +8,12 @@
 		<a href="http://cl.planetalinux.org/"><img src="images/banderas/cl.png"></a>&nbsp;&nbsp;
 		<a href="http://co.planetalinux.org/"><img src="images/banderas/co.png"></a>&nbsp;&nbsp;
 		<a href="http://sv.planetalinux.org/"><img src="images/banderas/sv.png"></a>&nbsp;&nbsp;
-		<a href="http://mx.planetalinux.org/"><img src="images/banderas/mx.png"></a>&nbsp;&nbsp;                
+		<a href="http://mx.planetalinux.org/"><img src="images/banderas/mx.png"></a>&nbsp;&nbsp;
+		<br />		
+		<a href="http://ni.planetalinux.org/"><img src="images/banderas/ni.png"></a>&nbsp;&nbsp;
 		<a href="http://pe.planetalinux.org/"><img src="images/banderas/pe.png"></a>&nbsp;&nbsp;
-		<a href="http://ve.planetalinux.org/"><img src="images/banderas/ve.png"></a>
+		<a href="http://ve.planetalinux.org/"><img src="images/banderas/ve.png"></a>&nbsp;&nbsp;
+		<a href="http://pa.planetalinux.org/"><img src="images/banderas/pa.png"></a>
 		<br />
 		<a href="http://planeta.debian.net"/><img src="images/banderas/debian.png"></a>
 		</div>
