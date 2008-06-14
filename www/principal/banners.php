ad81ea71229f088b8b6cb93000a122c745ac332d
maop <maop@b249013b-0064-4a1a-be8d-188746b68b0a>
2008-06-14 06:24:18 +0000

    arte
    git-svn-id: svn+ssh://menteslibres.org/home/planetalinux/svn@145 b249013b-0064-4a1a-be8d-188746b68b0a

--- www/principal/banners.php
+++ www/principal/banners.php
@@ -7,6 +7,13 @@
 <legend>Arte</legend>
 <p>Ayuda a Planeta Linux agregando un banner en tu sitio.</p>
   <div id="banderas">
+        <p><img src="images/arte/planeta.jpg"></p>
+        <p><img src="images/arte/pl_120x240.jpg" align="center"></p>
+        <p><img src="images/arte/pl_125x125.jpg" align="center"></p>
+        <p><img src="images/arte/pl_200x200.jpg" align="center"></p>
+        <p><img src="images/arte/pl2_125x125.jpg" align="center"></p>
+        <p><img src="images/arte/pl2_200x200.jpg" align="center"></p>
+        <p><img src="images/arte/pl_240x240.jpg" align="center"></p>
 	<p><img src="images/planetalinux.png" align="center"></p>
   </div>
 </fieldset>
