a054bd54603b7253a4b8b4957a3194ea8b7b5ce7
David Moreno <david@axiombox.com>
2008-12-30 13:32:19 -0500

    drop de exec

--- tools/update.rb
+++ tools/update.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/ruby
 
 Dir.chdir "#{ENV["HOME"]}/current"
-exec "git pull origin master"
+%x[git pull origin master]
 
 Dir.chdir "/var/www/planetalinux/git"
-exec "git pull origin master"
+%x[git pull origin master]
 
 Dir.chdir "#{ENV["HOME"]}/current/proc"
 
 skip_dirs = %w/inc universo/
-
 Dir["*/config.ini"].each do |ini|
-	next if skip_dirs.include?(ini.split('/').first)
-	
-	puts "Processing: #{ini}"
-	exec "planetplanet #{ini}"
+        next if skip_dirs.include?(ini.split('/').first)
+
+        puts "Processing: #{ini}"
+        %x[planetplanet #{ini}]
 end
+
 
