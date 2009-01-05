e4007e614cc6ab833e9caecd1b015162ac7c4007
David Moreno <david@axiombox.com>
2009-01-04 21:41:57 -0500

    check para feeds

---
+++ tools/feeds_report.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "rfeedparser"
+require "planetalinux"
+
+require "pp"
+
+PlanetaLinux.instances.each do |instance|
+	puts "#{instance}:"
+	PlanetaLinux.feeds_by_instance(instance).each_pair do |k, v|
+		v.each do |f|
+			fp = FeedParser.parse f
+
+			if k == "active"
+				if fp.bozo
+					puts " '#{f}' seems to be an invalid feed and it's currently active"
+				end
+			elsif k == "inactive"
+				unless fp.bozo
+					if fp.entries.size > 0
+						puts " '#{f}' is currently inactive but appears to be valid and it has entries"
+					else
+						puts " '#{f}' is currently inactive but appears to be valid but without entries"
+					end
+				end
+			else
+				puts "BUG ON PLANETLINUX.RB!"
+				exit
+			end
+		end
+	end
+end
+
--- tools/planetalinux.rb
+++ tools/planetalinux.rb
@@ -1,7 +1,38 @@
 require "ini"
 require "tempfile"
+require "pp"
 
 module PlanetaLinux
+
+	def self.instances
+		arr = []
+		Dir["#{File.expand_path(File.dirname(__FILE__))}/../proc/*/config.ini"].each do |i|
+			next if i =~ /universo\/config.ini/ 
+			arr.push(i.split('/')[-2])
+		end
+		return arr
+	end
+
+	def self.feeds_by_instance(instance)
+		feeds = {
+			"active" => [],
+			"inactive" => []
+		}
+		File.open("#{File.dirname(__FILE__)}/../proc/#{instance}/config.ini", "r").each do |line|
+			next unless line =~ /\[http/
+
+			line =~ /\[(.+?)\]/
+			url = $1
+
+			if line.lstrip =~ /^#/
+				feeds["inactive"].push(url)
+			else
+				feeds["active"].push(url)
+			end
+		end
+		feeds
+	end
+
   def self.get_all
     all = String.new
     str = Dir["#{File.expand_path(File.dirname(__FILE__))}/../proc/*/config.ini"].each do |i|
