8aee8d664f2b4f9d773302c0f96c07e8d2100dd7
David Moreno <david@axiombox.com>
2009-01-19 10:45:01 -0500

    cambio feed de Kodegeek

--- tools/feeds_report.rb
+++ tools/feeds_report.rb
@@ -10,28 +10,30 @@ PlanetaLinux.instances.each do |instance|
 	puts "#{instance}:"
 	PlanetaLinux.feeds_by_instance(instance).each_pair do |k, v|
 		v.each do |f|
-			begin
-				fp = FeedParser.parse f
-			rescue
-				next
-			end
+			Thread.new {
+				begin
+					fp = FeedParser.parse f
+				rescue
+					next
+				end
 
-			if k == "active"
-				if fp.bozo
-					puts " '#{f}' seems to be an invalid feed and it's currently active"
-				end
-			elsif k == "inactive"
-				unless fp.bozo
-					if fp.entries.size > 0
-						puts " '#{f}' is currently inactive but appears to be valid and it has entries"
-					else
-						puts " '#{f}' is currently inactive but appears to be valid but without entries"
+				if k == "active"
+					if fp.bozo
+						puts " '#{f}' seems to be an invalid feed and it's currently active"
 					end
+				elsif k == "inactive"
+					unless fp.bozo
+						if fp.entries.size > 0
+							puts " '#{f}' is currently inactive but appears to be valid and it has entries"
+						else
+							puts " '#{f}' is currently inactive but appears to be valid but without entries"
+						end
+					end
+				else
+					puts "BUG ON PLANETLINUX.RB!"
+					exit
 				end
-			else
-				puts "BUG ON PLANETLINUX.RB!"
-				exit
-			end
+			}
 		end
 	end
 end
