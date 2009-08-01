9834b633cdbb586b1b16c1c638872929736635d9
David Moreno <david@axiombox.com>
2009-08-01 12:44:16 +0200

    using feed_validator

--- tools/feeds_report.rb
+++ tools/feeds_report.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 require "rubygems"
-require "rfeedparser"
-require "planetalinux"
+require 'feed_validator'
+require File.join File.dirname(__FILE__), "planetalinux"
 
 require "pp"
 
@@ -10,30 +10,16 @@ PlanetaLinux.instances.each do |instance|
 	puts "#{instance}:"
 	PlanetaLinux.feeds_by_instance(instance).each_pair do |k, v|
 		v.each do |f|
-			Thread.new {
-				begin
-					fp = FeedParser.parse f
-				rescue
-					next
-				end
-
-				if k == "active"
-					if fp.bozo
-						puts " '#{f}' seems to be an invalid feed and it's currently active"
-					end
-				elsif k == "inactive"
-					unless fp.bozo
-						if fp.entries.size > 0
-							puts " '#{f}' is currently inactive but appears to be valid and it has entries"
-						else
-							puts " '#{f}' is currently inactive but appears to be valid but without entries"
-						end
-					end
-				else
-					puts "BUG ON PLANETLINUX.RB!"
-					exit
-				end
-			}
+      # puts "trying with #{f}"
+      val = W3C::FeedValidator.new
+      begin
+        val.validate_url f
+        unless val.valid?
+          puts " #{f} seems to be an invalid feed and it's currently #{k}"
+        end
+      rescue REXML::ParseException
+        puts " #{f} - timeout?"
+      end
 		end
 	end
 end
